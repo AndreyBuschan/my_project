bf3d1c8944aae339d5f4bc178bcee3b9dfad6c2e
Andrey <buschan.andrey@gmail.com>
2022-05-16 02:19:54 +0300

    add tasks 4, 5

---
+++ Lesson_26/js/classwork.js
@@ -0,0 +1,118 @@
+// Class work
+
+/* const month = prompt ('Enter month')
+
+switch (month) {
+  case '1':
+    alert('It`s Jan');
+    break;
+  case '2':
+    alert('It`s Feb');
+    break;
+  case '3':
+    alert('It`s Mar');
+    break;
+  case '4':
+    alert('It`s Apr');        
+    break;
+  default:
+    break;
+} */
+
+//Калькулятор
+/* const firstOperand = +prompt('Enter first operand');
+const secondOperand = +prompt('Enter second operand');
+const operator = prompt('Enter an operator');
+let result = null;
+
+switch (operator) {
+  case '+':
+    result = firstOperand + secondOperand;
+    break;
+  case '-':
+    result = firstOperand - secondOperand;
+    break;  
+  case '/':
+    result = firstOperand / secondOperand;
+    break;
+  case '*':
+    result = firstOperand * secondOperand;
+    break;  
+
+  default:
+    break;
+}
+
+alert('Result is ${result}'); */
+
+
+/* const num = 100;
+let i = 0;
+let msg = '';
+
+while (i < num) {
+  i++;
+
+  if (i % 3 === 0) {
+    msg = msg + ' #';
+  }
+}
+
+console.log(msg); */
+
+/* const num = prompt('Enter num!');
+let factorial = 1;
+
+while (i < num) {
+  i++;
+  factorial = factorial * i;
+}
+
+console.log(factorial); */
+
+
+
+/////////WHILE
+/* const num = +prompt('Enter num!');
+let i = num;
+
+while (i > 0) {
+  console.log(i);
+  i--;
+}
+
+alert(i) */
+
+
+
+//////////DO WHILE
+
+/* let i = 0;
+
+do {
+  i++;
+  console.log(i);
+} while (i < 10) */
+
+
+
+
+/* let num = 1000;
+let i = 0;
+
+do {
+  console.log(num)
+
+  i++;
+} while (num / 2 > 50)
+
+console.log(i); */
+
+
+/* const question = +prompt('2 + 2 * 2'); */
+
+/* do {
+  const question = +prompt('2 + 2 * 2');
+} while (question !== 6)
+console.log('OK'); */
+

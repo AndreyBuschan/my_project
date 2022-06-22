72ed4bf215654380af6352dd029abd891d442490
Andrey <buschan.andrey@gmail.com>
2022-06-22 17:47:58 +0300

    Project_2

---
+++ Lesson_39_Project_2/scripts/main.js
@@ -0,0 +1,49 @@
+document.querySelectorAll(".smooth-scroll").forEach((anchor) => {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+$(document).ready(() => {
+  $(".header__slider--container").slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    vertical: true,
+    dotsClass: "vertical-dots",
+  });
+});
+
+$(document).ready(() => {
+  $(".news__card--container").slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:
+      "<img class='news__arrow--prev slick-prev' src='images/prev-arrow.svg'>",
+    nextArrow:
+      "<img class='news__arrow--next  slick-next' src='images/next-arrow.svg'>",
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+
+
